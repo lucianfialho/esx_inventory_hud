135885b86153a27ef616c562fead4914e4d44572
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-12 04:30:56 -0300

    -- add dependencias e importando arquivos de configuracao do esx

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Boilerplate'
+
+server_scripts {
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'@es_extended/locales/en.lua',
+	'@es_extended/config.lua',
+	'@es_extended/config.weapons.lua',
+	'client/main.lua',
+}
+
+ui_page 'inventory_hud/dist/index.html'
+
+files {
+	'inventory_hud/dist/index.html'
+}
+
+dependencies {
+	'es_extended',
+}
