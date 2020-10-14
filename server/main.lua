7247588e533d2f61cbd7b43e7e84f384183877ea
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-13 23:08:39 -0300

    -- adicionando metodo que puxa as informacoes da arma

--- server/main.lua
+++ server/main.lua
@@ -21,3 +21,54 @@ ESX.RegisterServerCallback('esx_inventory_hud:GetPlayerPersonalData', function(p
     
     cb(playerPersonalData)
 end)
+
+ESX.RegisterServerCallback('esx_inventory_hud:GetPlayerWeaponData', function(playerId, cb)
+    local xPlayer, playerWeaponData  = ESX.GetPlayerFromId(playerId), {}
+
+    for k,v in ipairs(xPlayer.getLoadout()) do
+		local weaponHash = GetHashKey(v.value)
+        -- _,hudDamage,hudSpeed,hudCapacity,hudAccuracy,hudRange = GetWeaponHudStats(weaponHash)
+		table.insert(playerWeaponData, {
+            label = v.label,
+            count = 1,
+            type = 'item_weapon',
+            value = v.name,
+            usable = false,
+            rare = false,
+            ammo = v.ammo,
+            canGiveAmmo = (v.ammo ~= nil),
+            canRemove = true,
+            stats = {
+                damage = hudDamage,
+                fireRate = hudSpeed,
+                ammoCapacity = hudCapacity,
+                accuracy = chudAccuracy,
+                range = hudRange
+            }
+        })
+    end
+    
+    print(json.encode(playerWeaponData))
+    
+    cb(playerWeaponData)
+end)
+
+
+local function GetIntFromBlob(b, s, o)
+	r = 0
+	for i=1,s,1 do
+		r = r | (string.byte(b,o+i)<<(i-1)*8)
+	end
+	return r
+end
+
+function GetWeaponHudStats(weaponHash, none)
+	blob = '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'
+	retval = Citizen.InvokeNative(0xD92C739EE34C9EBA, weaponHash, blob, Citizen.ReturnResultAnyway())
+	hudDamage = GetIntFromBlob(blob,8,0)
+	hudSpeed = GetIntFromBlob(blob,8,8)
+	hudCapacity = GetIntFromBlob(blob,8,16)
+	hudAccuracy = GetIntFromBlob(blob,8,24)
+	hudRange = GetIntFromBlob(blob,8,32)
+	return retval, hudDamage, hudSpeed, hudCapacity, hudAccuracy, hudRange
+end
